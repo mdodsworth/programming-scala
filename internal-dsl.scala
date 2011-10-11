c5914f93d30884d883bca5ae83a9a922c7e3fb32
mdodsworth <dodster.michael@googlemail.com>
2011-10-10 22:42:12 -0700

    adding stupid dsl example

---
+++ internal-dsl.scala
@@ -0,0 +1,9 @@
+object galaxy {}
+object far {}
+object In {
+    def a(connector:Any) = this
+    def far(connector:Any) = this
+    def away = println("may the force be with you")
+}
+
+In a galaxy far far away
